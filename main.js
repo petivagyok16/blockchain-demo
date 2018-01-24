69e585b1fe5035b79647f443a4b3d9a1780230ca
peter palocz <peter.palocz91@gmail.com>
2018-01-24 21:12:44 +0100

    add main.js

---
+++ main.js
@@ -0,0 +1,65 @@
+const SHA256 = require('crypto-js/sha256');
+
+/**
+ * index: tells us where the block sits on the chain (optional)
+ * timestamp: when the block was created
+ * data: includes any data e.g. details of the transaction, how much money, who was the sender etc.
+ * previousHash: self explanatory
+ */
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2018", "Genesis block", "0")
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+let annaCoin = new Blockchain();
+annaCoin.addBlock(new Block(1, "23/01/2018", { amount: 4 }));
+annaCoin.addBlock(new Block(2, "24/01/2018", { amount: 6 }));
+// annaCoin.addBlock(new Block(1, "25/01/2018", { amount: 15 }));
+
+console.log(JSON.stringify(annaCoin, null, 4));
